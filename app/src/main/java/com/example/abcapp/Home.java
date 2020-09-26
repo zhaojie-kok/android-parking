3f5c388537755a2579bbb783f26b45fb7b899b64
Jun Xian <junxiantjx@gmail.com>
2020-09-27 04:59:58 +0800

    - Testing home page and button

--- app/src/main/java/com/example/abcapp/Home.java
+++ app/src/main/java/com/example/abcapp/Home.java
@@ -2,13 +2,37 @@ package com.example.abcapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Home extends AppCompatActivity {
+
+    private ImageButton button; // Start button on Home page
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Start button
+        button = findViewById(R.id.startBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMapActivity();
+            }
+        });
+
+
+    }
+
+    // Method to change activity to MapsActivity
+    public void openMapActivity() {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+
     }
 }
