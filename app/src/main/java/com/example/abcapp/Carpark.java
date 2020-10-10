9abe274a096e8bcd670c965652d6327a24fc6e70
rkoh-rq <49420412+rkoh-rq@users.noreply.github.com>
2020-10-10 19:09:35 +0800

    Update Carpark.java
    Changed LatLng to transient

--- app/src/main/java/com/example/abcapp/Carpark.java
+++ app/src/main/java/com/example/abcapp/Carpark.java
@@ -5,6 +5,9 @@ import com.google.android.gms.maps.model.LatLng;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public class Carpark implements Serializable {
@@ -12,7 +15,7 @@ public class Carpark implements Serializable {
     private String name;
     private double rate;
     private String carparkType;
-    private LatLng coordinates;
+    private transient LatLng coordinates;
 
     public Carpark(JSONObject carparkJSON) throws JSONException {
         this.carparkType = carparkJSON.getString("car_park_type");
@@ -27,5 +30,16 @@ public class Carpark implements Serializable {
 
     public LatLng getCoordinates() {
         return coordinates;
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+        out.writeDouble(coordinates.latitude);
+        out.writeDouble(coordinates.longitude);
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        coordinates = new LatLng(in.readDouble(), in.readDouble());
     }
 }
